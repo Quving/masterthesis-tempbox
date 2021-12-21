a8e0e1d6441a5ab5494d577499bf98a523ec9371
Vinh Ngu <vinh-ngu@hotmail.com>
2021-12-21 03:43:22 +0100

    Fix serializer.

--- measurements/serializers.py
+++ measurements/serializers.py
@@ -3,19 +3,8 @@ from rest_framework import serializers
 from measurements.models import Measurement
 
 
-class MeasurementSerializer(serializers.Serializer):
+class MeasurementSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
-    measurement_id = serializers.CharField(max_length=32)
-    lat = serializers.FloatField()
-    lng = serializers.FloatField()
-    source = serializers.CharField(max_length=64)
-    timezone = serializers.CharField(max_length=64)
-    timestamp = serializers.IntegerField()
-    temperature = serializers.FloatField()
-    country = serializers.CharField(max_length=64)
-    city = serializers.CharField(max_length=64)
-    street = serializers.CharField(max_length=64)
-    altitude = serializers.IntegerField()
 
     class Meta:
         model = Measurement
