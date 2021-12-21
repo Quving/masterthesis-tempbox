a0d3c8e0b04f699b98037a0b9a0e92d2bf9068e5
Vinh Ngu <vinh-ngu@hotmail.com>
2021-12-21 03:43:46 +0100

    Enable basic and session auth in rest_framework.

--- tempbox/settings.py
+++ tempbox/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS += [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 # Configure drf-yasg
