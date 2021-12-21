51a58b7a8288ae2f14ff875efe78861314832472
Vinh Ngu <vinh-ngu@hotmail.com>
2021-12-21 04:01:28 +0100

    Implement pagination.

---
+++ curl_test.sh
@@ -0,0 +1,3 @@
+curl -X GET "http://127.0.0.1:8000/measurements/" \
+    -H "accept: application/json" \
+    -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQwMzA4ODAyLCJpYXQiOjE2NDAwNDk2MDIsImp0aSI6IjViYjY5YTI1ZTg1NDQwNzBiODY2YjQ5MmI5NjdkMTg5IiwidXNlcl9pZCI6MX0.3Tq5VmGxTewIdcG6KHIAfJv7gIDhDprhy1QSTsfEmI0"
